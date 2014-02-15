df34e5d13fde2ce02f31ea3625c359d992bb9026
Pedro <pete@daffodil.uk.com>
2014-02-15 01:17:20 +0000

    start disection

---
+++ js/data.js
@@ -0,0 +1,15 @@
+
+/* This file defines the data sources and definitions
+ * 
+ */
+
+var icaoFileName = '../data/apt1000-icao.csv';
+var icaoGazetteer;
+var icaoIndex;
+
+var ilsFileName = '../apt1000/apt1000-ils.csv';
+var ilsGazetteer;
+
+var ilsStartPlace =  1; // San Francisco ~ but see below  //
+	
+	
---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*
+ * This file is loaded first for js..
+ * Contains all namespace and constants etc
+ * 
+ * TODO.. Add comments for all below.. 
+ * 
+ * Authors:  Theo, pedro
+ * 
+ */
+
+var PI = Math.PI;
+var PI05 = PI * 0.5;
+
+// degress
+var d2r = PI / 180;
+
+// radians
+var r2d = 180 / PI; 
+
